d5b664c7f575064d725e6167660e437cff67defb
Vidya939 <vidya@sunvision.co>
2025-04-29 14:30:58 +0530

    Delete industrial_safety_monitoring directory

--- industrial_safety_monitoring/compare.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-# Load the best-performing model
-model = YOLO("runs/detect/train/weights/best.pt")
-
-# OR load the last model (e.g., to resume training)
-model = YOLO("runs/detect/train/weights/last.pt")
-model.train(resume=True)
--- industrial_safety_monitoring/predict.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("runs/detect/train/weights/best.pt")
-model.predict(source="data/main.mp4", save=True, conf=0.25)
